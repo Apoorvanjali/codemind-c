47470422366952db8a90f24627a386aa451bf9c0
Apoorvanjali <119914856+Apoorvanjali@users.noreply.github.com>
2023-05-23 14:09:31 +0530

    #include<stdio.h> int main() {     int N,i;     scanf("%d",&N);     int arr[N];     arr[0]=0,arr[1]=1;     for(i=2;i<N;i++)     {         arr[i]=arr[i-1]+arr[i-2];     }     for(i=0;i<N;i++)     {         printf("%d ",arr[i]);     }     return 0;
    }

--- fibonacci.c
+++ fibonacci.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int i,x,y,n;
-    scanf("%d",&n);
-    x=0;
-    y=1;
-    printf("%d ",x);
-    while(n>1)
+    int N,i;
+    scanf("%d",&N);
+    int arr[N];
+    arr[0]=0,arr[1]=1;
+    for(i=2;i<N;i++)
     {
-        i=x;
-        x=x+y;
-        printf("%d ",x);
-        y=i;
-        n--;
+        arr[i]=arr[i-1]+arr[i-2];
     }
+    for(i=0;i<N;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+    
+    
 }
